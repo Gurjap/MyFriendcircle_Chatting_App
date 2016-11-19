d857c719a11ebe664806e861a3d97674a993dda7
Gurjap Singh <sjapgurjap@gmail.com>
2016-11-19 16:24:32 +0530

    Initial commit

---
+++ app/src/main/java/people/chat/mywhatsapp/Confirmation_page.java
@@ -0,0 +1,14 @@
+package people.chat.mywhatsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Confirmation_page extends AppCompatActivity {
+String mynumber="";//TODO :yournumber
+    String secret_code="";//TODO : call 9646271183 to get your key
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation_page);
+    }
+}
